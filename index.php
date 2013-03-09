304e461a040a3f796242ea92f3c513be258c67ca
CoskunYunusEmre <coshkhun@gmail.com>
2013-03-09 18:04:08 +0200

    index.php updated

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Usefull Login v2.0</title>
+        <?php
+            include 'meta.html';
+            include 'includes/title.html';
+        ?>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+
     </body>
 </html>
