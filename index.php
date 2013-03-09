2882d2e17ea500814c8c5ed9c6f6560a6a9a8926
CoskunYunusEmre <coshkhun@gmail.com>
2013-03-09 17:45:51 +0200

    Index.php title udpated.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Usefull Login v2.0</title>
     </head>
     <body>
         <?php
